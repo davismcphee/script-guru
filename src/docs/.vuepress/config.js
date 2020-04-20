83b3037c3532b5d3fd853df9954c24e5ac72129b
Davis McPhee <davis.mcphee@garvin-allen.com>
2020-04-19 22:55:07 -0300

    Fixing build directory

--- src/docs/.vuepress/config.js
+++ src/docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
   title: "ScriptGuru",
-  dest: path.join(__dirname, "../../../dist"),
+  dest: path.join(__dirname, "../../../docs"),
   themeConfig: {
     logo: "/assets/img/logo.svg",
     searchMaxSuggestions: 20,
